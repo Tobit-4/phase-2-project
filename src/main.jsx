393dc1b6642626c05cef84679e5f98b8b81aaa15
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-04-27 21:54:59 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './components/App'
-// import './index.css'
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import routes from "../src/routes";
-
-// const router = createBrowserRouter(routes);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(<RouterProvider router={router} />)
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
