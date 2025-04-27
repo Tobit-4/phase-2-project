75f72bcb855fb8b113b91d580b33cd25f1002d20
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-04-27 23:19:32 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    include: /\.(js|jsx)$/, 
-    babel: {
-      plugins: [
-        ['@babel/plugin-transform-react-jsx', {
-          runtime: 'automatic' 
-        }]
-      ]
-    }
-  })],
+  plugins: [
+    react({
+      include: /\.(js|jsx)$/,
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
+        ]
+      }
+    })
+  ],
   esbuild: {
     loader: 'jsx',
-  },
-  json: {
-    stringify: false 
+    include: /\.(js|jsx|ts|tsx)$/,
   },
   resolve: {
+    extensions: ['.js', '.jsx', '.json'],
     alias: {
       crypto: 'crypto-browserify'
     }
   },
-  base: "phase-2-project.github.io"
-})
+  base: "/phase-2-project.github.io/",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
